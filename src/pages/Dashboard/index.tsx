49a37dcb812541253e75a807c220485447682cd8
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2022-08-25 15:44:22 -0300

    tasks category

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,6 @@
 import Header from '../../components/Header';
 import SearchForm from '../../components/SearchForm';
-import { Container } from './styles';
+import { Container, TasksCategory, TasksCategoryContainer } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
@@ -9,6 +9,15 @@ const Dashboard: React.FC = () => {
 
       <Container>
         <SearchForm />
+
+        <TasksCategoryContainer>
+          <TasksCategory type="button" $variant="created" $amount={5}>
+            Tarefas criadas
+          </TasksCategory>
+          <TasksCategory type="button" $variant="done" $amount="3 de 5">
+            Concluídas
+          </TasksCategory>
+        </TasksCategoryContainer>
       </Container>
     </>
   );
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,7 +1,40 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { convertPixelToRem } from 'css-blocks-styled-components';
 
 export const Container = styled.div`
   max-width: ${convertPixelToRem(750)};
   margin: -24px auto 0;
+`;
+
+export const TasksCategoryContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: ${convertPixelToRem(64)};
+`;
+
+interface TasksCategoryProps {
+  $variant: 'created' | 'done';
+  $amount: number | string;
+}
+
+export const TasksCategory = styled.button<TasksCategoryProps>`
+  background: transparent;
+  border: 0;
+  font-weight: bold;
+  font-size: ${convertPixelToRem(14)};
+
+  ${({ $variant, $amount, theme }) => css`
+    color: ${$variant === 'created' ? theme.blue : theme.purple};
+
+    ::after {
+      content: '${$amount}';
+      border-radius: 999px;
+      padding: 2px 8px;
+      margin-left: 8px;
+      background: ${theme['gray-400']};
+
+      font-size: ${convertPixelToRem(12)};
+      color: ${theme['gray-100']};
+    }
+  `}
 `;
